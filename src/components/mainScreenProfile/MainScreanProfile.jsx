011f89508a0e47005d5e76e5d7c3a55addc42f11
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-05-26 14:59:54 +0300

    Add card User total Karma

--- src/components/mainScreenProfile/MainScreanProfile.jsx
+++ src/components/mainScreenProfile/MainScreanProfile.jsx
@@ -2,15 +2,19 @@ import styles from './MainScreanProfile.module.scss';
 import UserAchievements from './userAchievements/UserAchievements';
 import ProfileCard from './profileCard/ProfileCard.jsx';
 import UserGoal from './userGoal/UserGoal';
+import UserTotalKarma from './userTotalKarma/UserTotalKarma';
 
 const MainScreanProfile = () => {
 
     return(
         <div className={styles.mainScreanProfile}>
-            <ProfileCard/>
+            {/* <ProfileCard/>
             <div className={styles.mainScreanProfileCenter}>
                 <UserGoal/>
                 <UserAchievements/>
+            </div> */}
+            <div>
+                <UserTotalKarma/>
             </div>
         </div>
     )
---
+++ src/components/mainScreenProfile/userTotalKarma/UserTotalKarma.jsx
@@ -0,0 +1,25 @@
+import styles from './UserTotalKarma.module.scss';
+import Karma from '../../../img/user/karma.png';
+
+const UserTotalKarma = () => {
+
+    return(
+        <div className={styles.userTotalKarma}>
+            <div className={styles.userTotalKarmaHead}>
+                <p>Total Karma</p>
+            </div>
+            <div className={styles.userTotalKarmaAdress}>
+                <p>Coffe, Kyiv city, Khoryva street</p>
+            </div>
+            <div className={styles.userTotalKarmaItem}>
+                <img src={Karma} alt="Karma" />
+                <p>2300</p>
+            </div>
+            <div className={styles.userTotalKarmaBtn}>
+                <button>Сontest of the month</button>
+            </div>
+        </div>
+    )
+}
+
+export default UserTotalKarma;
