fe6e3028a274cb308d6d07bfc60f16e8789adcd9
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-06-09 19:26:05 +0300

    Add data user UserLevel

--- src/components/userProfileScreean/userLevel/UserLevel.jsx
+++ src/components/userProfileScreean/userLevel/UserLevel.jsx
@@ -5,55 +5,40 @@ import LvlTwo from "../../../img/Lvl/Lvl-2.png";
 import LvlThree from "../../../img/Lvl/Lvl-3.png";
 import LvlFour from "../../../img/Lvl/Lvl-4.png";
 
-const UserLevel = () => {
-    const progres = [
-        {
-            test: {
-                progresProc: 30
-            },
-            cups: {
-                progresProc: 40
-            },
-            checks: {
-                progresProc: 23
-            },
-            cappuccino: {
-                progresProc: 50
-            },
-            amountChecks: {
-                progresProc: 67
-            },
-        }
-    ];
+const UserLevel = ({ users }) => {
 
     return (
-        <div className={styles.userLevel}>
-            <div className={styles.userLevelImg}>
-                <img src={LvlOne} alt="level One" />
-                <img src={LvlTwo} alt="level Two" />
-                <img src={LvlThree} alt="level Three" />
-                <img src={LvlFour} alt="level Four" />
-            </div>
-            <div className={styles.userLevelNextLvl}>
-                <h2>To move to the second level you need:</h2>
-                <div className={styles.userLevelNextLvlDescr}>
-                    <ul>
-                        <li>Successfully pass the test</li>
-                        <UserWishesProgres progress={progres[0].test.progresProc} />
-                        <li>Brew 300 cups of coffee</li>
-                        <UserWishesProgres progress={progres[0].cups.progresProc} />
-                        <li>Knock out 300 checks</li>
-                        <UserWishesProgres progress={progres[0].checks.progresProc} />
-                    </ul>
-                    <ul>
-                        <li>Brew a cappuccino in 120 seconds</li>
-                        <UserWishesProgres progress={progres[0].cappuccino.progresProc} />
-                        <li>The amount of the checks must exceed 700 thousand hryvnias</li>
-                        <UserWishesProgres progress={progres[0].amountChecks.progresProc} />
-                    </ul>
+        <>
+            {users.map(user => (
+                <div className={styles.userLevel} key={user.id}>
+                    <div className={styles.userLevelImg}>
+                        <img src={LvlOne} alt="level One" />
+                        <img src={LvlTwo} alt="level Two" />
+                        <img src={LvlThree} alt="level Three" />
+                        <img src={LvlFour} alt="level Four" />
+                    </div>
+                    <div className={styles.userLevelNextLvl}>
+                        <h2>To move to the second level you need:</h2>
+                        <div className={styles.userLevelNextLvlDescr}>
+                            <ul>
+                                <li>Successfully pass the test</li>
+                                <UserWishesProgres progress={user.moveNextLevelProcent.successfullyPassTest} />
+                                <li>Brew 300 cups of coffee</li>
+                                <UserWishesProgres progress={(user.moveNextLevelProcent.brew300CupsOfCoffee/300*100)} />
+                                <li>Knock out 300 checks</li>
+                                <UserWishesProgres progress={(user.moveNextLevelProcent.knockOut300Checks/300*100)} />
+                            </ul>
+                            <ul>
+                                <li>Brew a cappuccino in 120 seconds</li>
+                                <UserWishesProgres progress={(user.moveNextLevelProcent.brewCappuccinoIn120Seconds/120*100)} />
+                                <li>The amount of the checks must exceed 200 thousand hryvnias</li>
+                                <UserWishesProgres progress={(user.moveNextLevelProcent.amountOfChecksExceed200K/200000*100)} />
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            ))}
+        </>
     );
 };
 
