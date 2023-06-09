3ea5cf8c97c50889695638f42007356c8cd84fe8
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-06-09 19:03:05 +0300

    Add data user for UserStatistics

--- src/components/userProfileScreean/userStatistics/UserStatistics.jsx
+++ src/components/userProfileScreean/userStatistics/UserStatistics.jsx
@@ -1,34 +1,38 @@
 import styles from "./UserStatistics.module.scss";
 
-const UserStatistics = () => {
+const UserStatistics = ({ users }) => {
 
     return (
-        <div className={styles.userStatisticsWrap}>
-            <div className={styles.userStatisticsHeader}>
-                <h2>Your statistics</h2>
-            </div>
-            <div className={styles.userStatisticsItem}>
-                <div className={styles.userStatisticsDescr}>
-                    <h2>61</h2>
-                    <p>Day worked out</p>
+        <>
+            {users.map(user => (
+                <div className={styles.userStatisticsWrap} key={user.id}>
+                    <div className={styles.userStatisticsHeader}>
+                        <h2>Your statistics</h2>
+                    </div>
+                    <div className={styles.userStatisticsItem}>
+                        <div className={styles.userStatisticsDescr}>
+                            <h2>{user.yourStatistics.dayWorkedOut}</h2>
+                            <p>Day worked out</p>
+                        </div>
+                        <div className={styles.line}></div>
+                        <div className={styles.userStatisticsDescr}>
+                            <h2>{user.yourStatistics.checksPunched}</h2>
+                            <p>Checks punched</p>
+                        </div>
+                        <div className={styles.line}></div>
+                        <div className={styles.userStatisticsDescr}>
+                            <h2>{user.yourStatistics.coffeeIsBrewed}</h2>
+                            <p>Coffee is brewed</p>
+                        </div>
+                        <div className={styles.line}></div>
+                        <div className={styles.userStatisticsDescr}>
+                            <h2>{user.yourStatistics.totalAmountOfChecks}</h2>
+                            <p>Total amount of checks</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.line}></div>
-                <div className={styles.userStatisticsDescr}>
-                    <h2>356</h2>
-                    <p>Checks punched</p>
-                </div>
-                <div className={styles.line}></div>
-                <div className={styles.userStatisticsDescr}>
-                    <h2>91</h2>
-                    <p>Coffee is brewed</p>
-                </div>
-                <div className={styles.line}></div>
-                <div className={styles.userStatisticsDescr}>
-                    <h2>210 673</h2>
-                    <p>Total amount of checks</p>
-                </div>
-            </div>
-        </div>
+            ))}
+        </>
     )
 }
 
