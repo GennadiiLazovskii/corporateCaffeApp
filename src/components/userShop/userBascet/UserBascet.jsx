1e6ca705aeea765e523bd28cb056734d1ae36e77
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2023-06-22 20:07:07 +0300

    Add order bascet

--- src/components/userShop/userBascet/UserBascet.jsx
+++ src/components/userShop/userBascet/UserBascet.jsx
@@ -1,10 +1,14 @@
-import styles from './UserBascet.module.scss';
-import Cross from '../../../img/header/cross.png';
-import Smile from '../../../img/shop/sad.png';
-import { setModalContext } from '../../../features/buttonExit/buttonExitSlice';
-import { useDispatch } from 'react-redux';
+import styles from "./UserBascet.module.scss";
+import Cross from "../../../img/header/cross.png";
+import Smile from "../../../img/shop/sad.png";
+import { setModalContext } from "../../../features/buttonExit/buttonExitSlice";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 const UserBascet = () => {
+  const [show, setShow] = useState(true);
+  const [show1, setShow1] = useState(false);
+
 
   const dispatch = useDispatch();
 
@@ -15,14 +19,41 @@ const UserBascet = () => {
           <img src={Cross} alt="Exit" />
         </button>
       </div>
-      <div className={styles.userBascetTitle}>
-        <h2>You have no orders yet</h2>
-      </div>
-      <div className={styles.userBascetImg}>
-        <img src={Smile} alt="Smile" />
-      </div>
+      {show && (
+        <>
+          <div className={styles.userBascetTitle}>
+            <h2>You have no orders yet</h2>
+          </div>
+          <div className={styles.userBascetImg}>
+            <img src={Smile} alt="Smile" />
+          </div>
+        </>
+      )}
+      {show1 && (
+        <>
+        <div className={styles.orderBascet}>
+          <div className={styles.orderBascetTitle}>
+            <h2>My orders</h2>
+          </div>
+          <div className={styles.orderBascetWrap}>
+            <div className={styles.orderBascetWrapImg}>
+              <img src="https://mkarapuz.com.ua/wp-content/uploads/2019/05/muzhskaya-futbolka-chernaya-1006.png" alt="shirt" />
+            </div>
+            <div className={styles.orderBascetWrapDescr}>
+              <p>Branded T-shirt "Coffee"</p>
+            </div>
+            <div className={styles.orderBascetWrapStatus}>
+              <p>In delivery</p>
+            </div>
+            <div className={styles.orderBascetWrapDate}>
+              <p>Will be delivered <br/>03/02/2023</p>
+            </div>
+          </div>
+        </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UserBascet
+export default UserBascet;
